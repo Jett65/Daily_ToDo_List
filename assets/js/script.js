cff5a56d3e727b31f1602547b6b93880e6568634
Jett <jettc505@gmail.com>
2022-10-18 12:50:46 -0600

    made all timebolcks work

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,17 +14,20 @@ class TimeBlock {
         // style div
         $(`.${this.name}`).css("display", "flex");
         $(`.${this.name}`).css("justify-content", "center");
-        $(`.${this.name}`).css("margin-bottom", "5px");
+        $(`.${this.name}`).css("margin-bottom", "3px");
         // style time
         $(`#time${this.name}`).css("border-top", "dotted");
         $(`#time${this.name}`).css("border-bottom", "dotted");
+        $(`#time${this.name}`).css("border-right", "solid");
+        $(`#time${this.name}`).css("aline-content", "center");
         $(`#time${this.name}`).css("margin", "0%");
         $(`#time${this.name}`).css("padding", "5px");
         $(`#time${this.name}`).css("padding-left", "40px");
+        $(`#time${this.name}`).css("min-width", "90px");
         // style inputBox
         $(`#inputBox${this.name}`).css("padding", "25px");
         $(`#inputBox${this.name}`).css("border", "none");
-        $(`#inputBox${this.name}`).css("padding-right", "70%");
+        $(`#inputBox${this.name}`).css("width", "70%");
         // style saveBtn
         $(`#saveBtn${this.name}`).css("padding-left", "4%");
         $(`#saveBtn${this.name}`).css("padding-right", "4%");
@@ -34,47 +37,80 @@ class TimeBlock {
         $(`#saveBtn${this.name}`).css("border-bottom-right-radius", "10%");
         // Checks if there is anything in localStorage
         var savedData = localStorage.getItem(this.name);
-        if (savedData){
-            console.log("True")
+        if (savedData) {
+            var pareedData = JSON.parse(savedData);
+            $(`#inputBox${this.name}`).val(pareedData["inputVal"]);
         }
     }
 
     statusColor() {
         // Change the color of the timeblock for the past, present or future
         var currentTime = moment().format("lll");
-        var dowDateTime = moment(`${this.date} ${this.time}`);
-        if (dowDateTime.isAfter(currentTime)) {
+        var dateWithTime = moment(`${this.date} ${this.time}`);
+        if ($(`#inputBox${this.name}`).val() == "") {
             $(`#inputBox${this.name}`).css("background-color", "lightgray");
-        } else if (dowDateTime.isBefore(currentTime)) {
-            $(`#inputBox${this.name}`).css("background-color", "rgb(255, 90, 90)");
         } else {
-            $(`#inputBox${this.name}`).css("background-color", "rgb(244, 255, 91)");
+            if (dateWithTime.isAfter(currentTime)) {
+                $(`#inputBox${this.name}`).css("background-color", "lightgreen");
+            } else if (dateWithTime.isBefore(currentTime)) {
+                $(`#inputBox${this.name}`).css("background-color", "rgb(255, 90, 90)");
+            } else if (dateWithTime.isSame(currentTime)) {
+                $(`#inputBox${this.name}`).css("background-color", "rgb(244, 255, 91)");
+            } else {
+                $(`#inputBox${this.name}`).css("background-color", "lightgray");
+            }
         }
     }
-    onClick () {
+
+    onClick() {
+        // Called to check if the batten is clicked
         var thName = this.name;
-        var objs = {
-            inputVal: $(`#inputBox${this.name}`).val(),
-            date: this.date
-        };
+        var thDate = this.date;
         $(`#saveBtn${this.name}`).click(function () {
+            var objs = {
+                inputVal: $(`#inputBox${thName}`).val(),
+                date: thDate
+            };
             localStorage.setItem(thName,
-                JSON.stringify(objs) 
-            )
-            console.log(objs)
-            
+                JSON.stringify(objs)
+            );
+            console.log(localStorage.getItem(thName));
         });
     }
-
 }
 
-var test = new TimeBlock("9am", "9:00 AM", moment().format("MMM D Y"));
-test.displayBlocks()
-test.statusColor()
-test.onClick()
 
 // Displays the current Day date
 $('#currentDay').text(moment().format("MMMM Do YYYY"));
 
-// TODO: Make the this.date save the date to localStorage when the saveBtn is clicked
-// TODO: Make a hover for the Btn
+// Displays the time blocks ----
+
+function callTieBlock(inst) {
+    // Calls the time functions for the given instance of  timeBlock\
+    inst.displayBlocks();
+    inst.statusColor();
+    inst.onClick();
+}
+
+//gets the current date
+var date = moment().format("MMM DD YYYY");
+
+// TimeBlock instancies
+var nineAM = new TimeBlock("9am", "9:00 AM", date,);
+callTieBlock(nineAM);
+var tenAM = new TimeBlock("10am", "10:00 AM", date,);
+callTieBlock(tenAM);
+var elevenAM = new TimeBlock("11am", "11:00 AM", date,);
+callTieBlock(elevenAM);
+var twelvePM = new TimeBlock("12am", "12:00 PM", date,);
+callTieBlock(twelvePM);
+var onePM = new TimeBlock("1PM", "1:00 PM", date,);
+callTieBlock(onePM);
+var twoPM = new TimeBlock("2PM", "2:00 PM", date,);
+callTieBlock(twoPM);
+var threePM = new TimeBlock("3PM", "3:00 PM", date,);
+callTieBlock(threePM);
+var fourPM = new TimeBlock("4PM", "4:00 PM", date,);
+callTieBlock(fourPM);
+var fivePM = new TimeBlock("5PM", "5:00 PM", date,);
+callTieBlock(fivePM);
